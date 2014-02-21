e4440193f1ed7cbead4e63d4e3d7fc2a3bb0c975
Paul Mandel <paul.mand3l@gmail.com>
2014-02-20 22:37:34 -0800

    Adding roles Registering models with admin site Switching up permissions, etc.

--- organizer/models.py
+++ organizer/models.py
@@ -1,6 +1,8 @@
 import recurrence.fields
 
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
 
 from django_extensions.db import fields
 
@@ -9,27 +11,27 @@ class Organization(models.Model):
     name = models.CharField(max_length=200)
     auto_slug = fields.AutoSlugField(populate_from='name')
 
-    founded = models.DateField('date organization was founded')
+    founded = models.DateField('date organization was founded', blank=True, null=True)
 
 class Venue(models.Model):
     name = models.CharField(max_length=200)
     auto_slug = fields.AutoSlugField(populate_from='name')
 
-    organization = models.ForeignKey(Organization)
+    organization = models.ForeignKey(Organization, related_name="venues")
 
     # Used when automatically generating Dance objects
     default_price = models.IntegerField(blank=True, null=True)
     default_price_low = models.IntegerField(blank=True, null=True)
     default_price_high = models.IntegerField(blank=True, null=True)
 
-    default_start = models.DateTimeField(blank=True, null=True)
-    default_end = models.DateTimeField(blank=True, null=True)
+    default_start = models.TimeField(blank=True, null=True)
+    default_end = models.TimeField(blank=True, null=True)
 
     recurrence_type = recurrence.fields.RecurrenceField()
 
 class Dance(models.Model):
-    venue = models.ForeignKey(Venue)
-    organization = models.ForeignKey(Organization)
+    venue = models.ForeignKey(Venue, related_name="dances")
+    organization = models.ForeignKey(Organization, related_name="dances")
 
     auto_slug = fields.AutoSlugField(populate_from=['venue', 'start'])
 
@@ -42,3 +44,22 @@ class Dance(models.Model):
     start = models.DateTimeField()
     end = models.DateTimeField()
     #######
+
+class RoleType(models.Model):
+    name = models.CharField(max_length=100)
+
+class Role(models.Model):
+    dance = models.ForeignKey(Dance, blank=True, null=True, related_name="staff")
+    venue = models.ForeignKey(Venue, blank=True, null=True, related_name="staff")
+    organization = models.ForeignKey(Organization, blank=True, null=True, related_name="staff")
+
+    user = models.ForeignKey(User, related_name="roles")
+
+    role_type = models.ForeignKey(RoleType, related_name="roles")
+
+
+admin.site.register(Organization)
+admin.site.register(Venue)
+admin.site.register(Dance)
+admin.site.register(RoleType)
+admin.site.register(Role)
